3ae7fca19847f83b2b210e4496b6fee8e5b4997f
julian.rofrano <julian.rofrano@julian.rofrano-07322>
2019-09-04 10:53:03 -0300

    add coverrage and coveralls

--- events/tests.py
+++ events/tests.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 from .utils import get_api_event_id, get_api_events, get_auth_token
 import json
 from unittest.mock import patch
-with open("/Users/julian.rofrano/eventbrite/todolisteb/todolist/events/res.json", "r") as read_file:
-    data = json.load(read_file)
 
 
 # class TestApiUtils(TestCase):
